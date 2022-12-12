8c5ffa3783675951609c114f1e8e9dc6886f931d
kano <kano7101d@gmail.com>
2022-12-12 15:57:50 +0900

    空気から木材への熱量について（不完全）

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 from math import log10
 import numpy as np
 import matplotlib.pyplot as plt
-from functions import get_T_fu
-from config import N_CELL, T_WD_0
+from functions import get_T_fu, get_h_air_wd, get_lambda_wd, get_rho_c_wd
+from config import N_CELL, T_0_WD, ALPHA_WD, D_t, D_X, q_FU, q_GEN
 
 t = 0  # time秒
 T_fu = get_T_fu(t)  # 炉内温度℃
 
 #木材の初期化
-T_wd = np.array([T_WD_0] * N_CELL)
-print(T_wd[0])
+T_0_wd = np.array([T_0_WD] * N_CELL)
+T_1_wd = np.array([0] * N_CELL)
+T_1_wd2 = np.array([0] * N_CELL)
+h_air = get_h_air_wd(T_0_wd[0])
+lambda_wd = get_lambda_wd(T_0_wd[0])
+rho_c_wd = get_rho_c_wd(T_0_wd[0])
+T_fu = get_T_fu(0)
+print(lambda_wd, h_air)
+print((D_X / lambda_wd) * q_FU)
+print(ALPHA_WD * D_t / D_X**2)
+#T_1_wd[0] = T_0_wd[0] + (ALPHA_WD * D_t / D_X**2) * (T_0_wd[1] + D_X * h_air / lambda_wd * T_fu - (1 + (D_X * h_air / lambda_wd)) * T_0_wd[0] + ((D_X**2) / lambda_wd) * q_GEN + (D_X / lambda_wd) * q_FU)
+#iPadで算出した空気から木材への流入における式 TODO:内部発熱(最後から2番目の項）を0とした場合（炭化前？）
+T_1_wd2[0] = T_0_wd[0] + (ALPHA_WD * D_t / (D_X**2)) * (T_0_wd[1] - T_0_wd[0]) + (D_t * h_air / (D_X * rho_c_wd)) * (T_fu - T_0_wd[0]) + D_t * q_FU / (D_X * rho_c_wd) + D_t * q_GEN / rho_c_wd
+#iPadで算出した左はじの式 TODO:内部発熱(最後の項）を考慮する（炭化について）TODO:輻射（最後から2番目の項）の考え方、おそらく違うので再考
+
+print(T_0_wd)
+print(T_1_wd)
+print(T_1_wd2)
+#for x in range(N_CELL):
 
 #x = np.arange(0.0, 120.0 * 60, 1.0)
 # plt.plot(x, T_fu(x), 'b:')
 # plt.show()
-
-
-def T_wd2(t):
-    return
-
-
-"""
-参考値
-T=20
-
-c = 1133 + 4.853*(T-373.15)
-
-c_u =  (u/100 + c_0)/(u/100 + 1)
-
-λ=0.09
-λ=1.163*(0.022 + 0.724*d_0/1000 + 0.0931*(d_0/1000)^2)
-λ_2 = λ_1*(1-k*(u_1-u_2))
-
-λ_2 = λ_1*(1-(1.1-0.00098*d_0))*(T_1-T_2)/100
-"""
