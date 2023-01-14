82902b0260a212fa0c44a4f624bbda18cbc2901d
kano <kano7101d@gmail.com>
2023-01-14 20:18:13 +0900

    シミュレーターver.1.2（100℃で分岐、輻射考慮まだ）

--- functions.py
+++ functions.py
@@ -24,7 +24,6 @@ def get_h_air_wd(T_wd):
         W/m²*K: 熱伝達率
     """
     return 0.168 * T_wd + 4.64
-    #TODO:入力（温度T）の単位と出力（熱伝達率h）の単位が合ってるか、値が変になってないか確認
     #考察:引用:由田、式9,10、加熱面,非加熱面同じ式のため、非加熱面ではあるが空気と木材の式10を採用、式中の温度は低温度側の物質の温度を採用してそう？もしくは着目する物体？
 
 
@@ -41,7 +40,23 @@ def get_lambda_wd(T_wd, T1_wd=20, lambda1=0.09, rho_wd_0=RHO_WD_0):
         W/m*K:求めたい木材の熱伝導率 
     """
     return lambda1 * (1 - (1.1 - 0.00098 * rho_wd_0) * (T1_wd - T_wd) / 100)
-    #考察:引用の式がおそらく違うため、改編版引用:木材の物理p63(4-12)
+    #TODO:高温域まで対応してる式かどうか#考察:引用の式がおそらく違うため、改編版引用:木材の物理p63(4-12)
+
+
+def get_lambda_wd_0(T_wd, T1_wd=20, rho_wd_0=RHO_WD_0):
+    """全乾状態の木材の温度から熱伝導率を求める関数
+
+    Args:
+        T_wd (℃): 熱伝導率を求める時の木材の温度
+        T1_wd (℃, optional): 熱伝導率の参考値の時の木材の温度. Defaults to 20.
+        rho_wd_0 (kg/m³, optional): 木材の全乾密度. Defaults to RHO_WD_0.
+
+    Returns:
+        W/m*K:求めたい木材の熱伝導率
+    """
+    lambda20 = 1.163 * (0.022 + 0.724 * (rho_wd_0 / 1000) + 0.0931 * ((rho_wd_0 / 1000)**2))
+    return lambda20 * (1 - (1.1 - 0.00098 * rho_wd_0) * (T1_wd - T_wd) / 100)
+    #考察:式4-09を使ってるけど、繊維方向の可能性もある？#引用:木材の物理（緑）p67
 
 
 def get_rho_c_wd(T_wd):
@@ -57,9 +72,23 @@ def get_rho_c_wd(T_wd):
     return lambda_wd / ALPHA_WD
 
 
-# print(get_T_fu(1))
+def get_rho_c_wd_0(T_wd):
+    """全乾状態の木材の温度から密度ρ*比熱cを求める式
 
-# print(get_lambda_wd(30))
+    Args:
+        T_wd (℃): 木材の温度
+
+    Returns:
+        J/m³*K: 密度ρ*比熱c
+    """
+    lambda_wd = get_lambda_wd_0(T_wd)
+    return lambda_wd / ALPHA_WD
+
+
+# print(get_T_fu(1))
+# print(get_lambda_wd(200))
+# print(get_lambda_wd_0(200))
+# print(0.0029 * 150 + 0.01954)
 # print(0.00275 * 21 + 0.09319)
 # print(get_lambda_wd(21, 20, 0.14, 0.38))
 """
--- main.py
+++ main.py
@@ -1,30 +1,56 @@
-from math import log10
 import numpy as np
 import matplotlib.pyplot as plt
-from functions import get_T_fu, get_h_air_wd, get_lambda_wd, get_rho_c_wd
-from config import N_CELL, T_0_WD, ALPHA_WD, D_t, D_X, q_FU, q_GEN, N_TIME
+from functions import get_T_fu, get_h_air_wd, get_lambda_wd, get_rho_c_wd, get_lambda_wd_0, get_rho_c_wd_0
+from config import N_CELL, T_0_WD, ALPHA_WD, D_t, D_X, q_FU, q_GEN, N_TIME, LENGTH, TIME_END
 
-#木材の初期化
-T_wd = np.array([[T_0_WD] * (N_CELL + 1)] * (N_TIME + 1))
-#T_wd[t:経過時間(0秒~)][x:場所0m~]
+x_axis = np.arange(0.0, LENGTH + D_X, D_X)
 
-for t in range(1, N_TIME + 1):
+# x_axis = []
+y_axis = []
+
+for n in range(N_TIME + 1):
+    t = n * D_t  #現実の経過時間
+    # x_axis.append(t)
     T_fu = get_T_fu(t)
-    for x in range(N_CELL + 1):
-        h_air = get_h_air_wd(T_wd[t][x])
-        lambda_wd = get_lambda_wd(T_wd[t][x])
-        rho_c_wd = get_rho_c_wd(T_wd[t][x])
+    # y_axis.append(T_fu)
+    if n == 0:
+        print('start')
+        #木材温度の初期化
+        T_wd = np.array([[T_0_WD] * (N_CELL + 1)] * (N_TIME + 1))
+        #T_wd[t:経過時間(0秒~)][x:場所0m~]
+    else:
+        for x in range(N_CELL + 1):
 
-        if x == 0:
-            T_wd[t][0] = T_wd[t - 1][0] + (ALPHA_WD * D_t / (D_X**2)) * (T_wd[t - 1][1] - T_wd[t - 1][0]) + (D_t * h_air / (D_X * rho_c_wd)) * (T_fu - T_wd[t - 1][0]) + (D_t * q_FU) / (D_X * rho_c_wd) + (D_t * q_GEN) / rho_c_wd
-            #iPadで算出した左はじの式 TODO:内部発熱(最後の項）を考慮する（炭化について）TODO:輻射（最後から2番目の項）の考え方、おそらく違うので再考
-        elif 0 < x < N_CELL:
-            T_wd[t][x] = T_wd[t - 1][x] + (ALPHA_WD * D_t / (D_X**2)) * (T_wd[t - 1][x - 1] + T_wd[t - 1][x + 1] - 2 * T_wd[t - 1][x]) + (D_t * q_GEN) / rho_c_wd
-            #TODO:内部発熱(最後の項）を考慮する（炭化について）
-        elif x == N_CELL:
-            T_wd[t][N_CELL] = T_wd[t - 1][N_CELL] + (ALPHA_WD * D_t / (D_X**2)) * (T_wd[t - 1][N_CELL - 1] - T_wd[t - 1][N_CELL]) - (D_t * h_air / (D_X * rho_c_wd)) * (T_wd[t - 1][N_CELL] - T_0_WD) + (D_t * q_GEN) / rho_c_wd
-            #TODO:内部発熱(最後の項）を考慮する（炭化について）DO:難燃処理層最後尾の次をどう考えるか（荷重支持部表面？長田さんは炉内温度？一面加熱だから、布施さんの外温と同じ？）
-    print(t, T_fu, T_wd[t])
+            h_air = get_h_air_wd(T_wd[n - 1][x])
+            if T_wd[n - 1][x] <= 100:
+                lambda_wd = get_lambda_wd(T_wd[n - 1][x])
+                rho_c_wd = get_rho_c_wd(T_wd[n - 1][x])
+            else:
+                lambda_wd = get_lambda_wd_0(T_wd[n - 1][x])
+                rho_c_wd = get_rho_c_wd_0(T_wd[n - 1][x])
+
+            if x == 0:
+                T_wd[n][0] = T_wd[n - 1][0] + (ALPHA_WD * D_t / (D_X**2)) * (T_wd[n - 1][1] - T_wd[
+                    n - 1][0]) + (D_t * h_air / (D_X * rho_c_wd)) * (T_fu - T_wd[n - 1][0]) + (
+                        D_t * q_FU) / (D_X * rho_c_wd) + (D_t * q_GEN) / rho_c_wd
+                #DO:差分法の実例がある本探す　#iPadで算出した左はじの式 TODO:内部発熱(最後の項）を考慮する（炭化について）TODO:輻射（最後から2番目の項）の考え方、おそらく違うので再考
+            elif 0 < x < N_CELL:
+                T_wd[n][x] = T_wd[n - 1][x] + (ALPHA_WD * D_t / (D_X**2)) * (T_wd[n - 1][
+                    x - 1] + T_wd[n - 1][x + 1] - 2 * T_wd[n - 1][x]) + (D_t * q_GEN) / rho_c_wd
+                #TODO:内部発熱(最後の項）を考慮する（炭化について）
+            elif x == N_CELL:
+                T_wd[n][N_CELL] = T_wd[n - 1][N_CELL] + (ALPHA_WD * D_t / (D_X**2)) * (
+                    T_wd[n - 1][N_CELL - 1] - T_wd[n - 1][N_CELL]
+                ) - (D_t * h_air /
+                     (D_X * rho_c_wd)) * (T_wd[n - 1][N_CELL] - T_0_WD) + (D_t * q_GEN) / rho_c_wd
+                #TODO:内部発熱(最後の項）を考慮する（炭化について）DO:難燃処理層最後尾の次をどう考えるか（荷重支持部表面？長田さんは炉内温度？一面加熱だから、布施さんの外温と同じ？）
+    # y_axis.append(T_wd[n][10])
+    # print(t, T_fu, T_wd[n])
+    #print(T_wd)
+y_axis = T_wd[N_TIME]
+
+plt.plot(x_axis, y_axis, "b:")
+plt.show()
 #T_1_wd[0] = T_0_wd[0] + (ALPHA_WD * D_t / D_X**2) * (T_0_wd[1] + D_X * h_air / lambda_wd * T_fu - (1 + (D_X * h_air / lambda_wd)) * T_0_wd[0] + ((D_X**2) / lambda_wd) * q_GEN + (D_X / lambda_wd) * q_FU)
 #iPadで算出した空気から木材への流入における式 TODO内部発熱(最後から2番目の項）を0とした場合（炭化前？）
 # T_1_wd[1] = T_0_wd[1] + (ALPHA_WD * D_t / (D_X**2)) * (T_0_wd[0] + T_0_wd[2] - 2 * T_0_wd[1]) + (D_t * q_GEN) / rho_c_wd
